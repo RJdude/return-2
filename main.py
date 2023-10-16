1701bb2bbf53ef62c13c2fef1bb03cd949a5712c
ROMAINJEANTY <26436408-ROMAINJEANTY@users.noreply.replit.com>
2023-10-16 12:25:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def Multiply(num1, num2):
+  return num1 * num2
+
+
+def shopping():
+  jeansPrice = 40
+  amountPairs = 6
+
+  total = Multiply(jeansPrice, amountPairs)
+  print("total price is", total)
+
+
+def episodes():
+  watched = 12
+  time = 50
