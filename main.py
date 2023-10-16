99a176b8b57f9454dd2989b969c18d629fa9975f
RJdude <148071166+RJdude@users.noreply.github.com>
2023-10-16 12:39:08 +0000

    I changed so cry

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 def Multiply(num1, num2):
-  return num1 * num2
-
-
+  return (num1 * num2)
 def shopping():
   jeansPrice = 40
   amountPairs = 6
 
   total = Multiply(jeansPrice, amountPairs)
   print("total price is", total)
-
-
 def episodes():
   watched = 12
   time = 50
